2a49b87fa18932d467ec8763a367d4eb8060fec6
Glebson Santos <glebonsantos@outlook.pt>
2025-04-13 16:31:55 -0300

    feat: add user dto validation

--- src/users/dtos/updateUser.dto.ts
+++ src/users/dtos/updateUser.dto.ts
@@ -1,5 +1,21 @@
+import { Transform } from "class-transformer";
+import { IsEmail, IsNumber, IsOptional, IsString, MinLength } from "class-validator";
+
 export class UpdateUserDto{
+    @IsNumber()
+    id: number;
+
+    @IsString()
+    @Transform(({value}) => value.trim())
+    @MinLength(3)
     name: string;
+
+    @IsEmail()
     email: string;
+    
+    @IsOptional()
+    @IsString()
+    @Transform(({value}) => value.trim())
+    @MinLength(6)
     password?: string;
 };
