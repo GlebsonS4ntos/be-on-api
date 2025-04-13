5e77bbd081d6c66d8e873dbaba0b94bc573eaf6b
Glebson Santos <glebonsantos@outlook.pt>
2025-04-12 22:52:48 -0300

    feat: typeorm config

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,10 +3,26 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { AuthModule } from './auth/auth.module';
 import { UsersModule } from './users/users.module';
+import { DataSource } from 'typeorm';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 
 @Module({
-  imports: [AuthModule, UsersModule],
+  imports: [AuthModule, UsersModule, ConfigModule.forRoot({
+    isGlobal: true
+  })],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [{
+    provide: 'DATA_SOURCE',
+    useFactory: async (config : ConfigService) => {
+      const dataSource = new DataSource({
+        type: 'postgres',
+        url: config.get<string>("DB_CONNECTION"),
+        entities: [__dirname + '/**/*.entity{.ts,.js}'],
+        synchronize: true
+      })
+      return dataSource.initialize()
+    },
+    inject: [ConfigService]
+  },AppService],
 })
 export class AppModule {}
