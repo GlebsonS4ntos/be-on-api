475859050bbc2afa290ea123beb1e33698dcd408
Glebson Santos <glebonsantos@outlook.pt>
2025-04-12 23:05:29 -0300

    feat: user typeorm config

--- src/users/entities/user.entity.ts
+++ src/users/entities/user.entity.ts
@@ -1,8 +1,18 @@
+import { Column, CreateDateColumn, Entity, Index, PrimaryGeneratedColumn } from "typeorm";
+
+@Entity('users')
 export class UserEntity {
+    @PrimaryGeneratedColumn()
     id: number;
+    @Column()
     name: string;
+    @Index({ unique: true })
+    @Column()
     email: string;
+    @Column()
     password: string;
+    @CreateDateColumn()
     createdAt: Date;
+    @Column({ default: false })
     isDeleted: boolean;
 }
